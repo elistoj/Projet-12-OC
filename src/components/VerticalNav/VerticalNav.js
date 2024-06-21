055836ae43c96ac36659f80af2d1d1823937d858
elistoj <atanastojaneli@gmail.com>
2024-06-21 20:35:46 +0200

    Update VerticalNav.js

--- src/components/VerticalNav/VerticalNav.js
+++ src/components/VerticalNav/VerticalNav.js
@@ -6,15 +6,34 @@ import weightsIcon from '../../images/icons/weights.png';
 import cyclingIcon from '../../images/icons/cycling.png';
 
 const VerticalNav = () => {
+  const handleSwimmingClick = () => {
+    console.log("Swimming button clicked");
+  };
+
+  const handleMeditationClick = () => {
+    console.log("Meditation button clicked");
+  };
+
+  const handleWeightsClick = () => {
+    console.log("Weights button clicked");
+  };
+
+  const handleCyclingClick = () => {
+    console.log("Cycling button clicked");
+  };
+
   return (
-    <nav className="vertical-nav">
-      <ul>
-        <li><img src={swimmingIcon} alt="Swimming" className="nav-icon" /></li>
-        <li><img src={meditationIcon} alt="Meditation" className="nav-icon" /></li>
-        <li><img src={weightsIcon} alt="Weights" className="nav-icon" /></li>
-        <li><img src={cyclingIcon} alt="Cycling" className="nav-icon" /></li>
-      </ul>
-    </nav>
+    <div>
+      <nav className="vertical-nav">
+        <ul>
+          <li><button className="nav-button" onClick={handleSwimmingClick}><img src={swimmingIcon} alt="Swimming" className="nav-icon" /></button></li>
+          <li><button className="nav-button" onClick={handleMeditationClick}><img src={meditationIcon} alt="Meditation" className="nav-icon" /></button></li>
+          <li><button className="nav-button" onClick={handleWeightsClick}><img src={weightsIcon} alt="Weights" className="nav-icon" /></button></li>
+          <li><button className="nav-button" onClick={handleCyclingClick}><img src={cyclingIcon} alt="Cycling" className="nav-icon" /></button></li>
+        </ul>   
+        <p className="copyright">Copyright, SportSee 2020</p>
+      </nav>
+    </div>
   );
 };
 
