b80ce54c3e372e3a8d718e9cfbaf0dd838fd073e
elistoj <atanastojaneli@gmail.com>
2024-07-01 15:16:31 +0200

    Update Profile.js

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from 'react';
 import './Profile.css';
 import ActivityChart from '../charts/ActivityChart/ActivityChart';
 import AverageSessionsChart from '../charts/AverageSessionsChart/AverageSessionsChart';
-import { fetchUserData, fetchUserActivity, fetchUserAverageSessions } from '../fetchFunctions/fetchFunctions';
+import { fetchUserData, fetchUserActivity, fetchUserAverageSessions, fetchUserPerformance } from '../fetchFunctions/fetchFunctions';
 import NutritionalInfo from '../charts/NutritionalInfo/NutritionalInfo';
+import RadarChart from '../charts/PerformanceChart/RadarChart';
+import ScoreChart from '../charts/ScoreChart/ScoreChart';
 
 const Profile = ({ userId }) => {
   const [userData, setUserData] = useState(null);
   const [userActivity, setUserActivity] = useState(null);
   const [userAverageSessions, setUserAverageSessions] = useState(null);
+  const [userPerformance, setUserPerformance] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,10 +19,12 @@ const Profile = ({ userId }) => {
         const data1 = await fetchUserData(userId);
         const data2 = await fetchUserActivity(userId);
         const data3 = await fetchUserAverageSessions(userId);
+        const data4 = await fetchUserPerformance(userId);
 
         setUserData(data1);
         setUserActivity(data2);
         setUserAverageSessions(data3);
+        setUserPerformance(data4);
       } catch (error) {
         console.error('Error fetching data:', error);
         // Handle error state if needed
@@ -29,22 +34,31 @@ const Profile = ({ userId }) => {
     fetchData();
   }, [userId]);
 
-  if (!userData || !userActivity || !userAverageSessions) return <div>Loading...</div>;
+  if (!userData || !userActivity || !userAverageSessions || !userPerformance) return <div>Loading...</div>;
+
+
 
   return (
     <div className="profile-container">
       <div className='profile-header-section'>
-      <div className="profile-header">
-        <h1>Bonjour <span className="user-name">{userData.userInfos.firstName}</span></h1>
-        <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
-      </div>
-      <div className="section">
-        <ActivityChart data={userActivity.sessions} />
-      </div>
-     
-      <div className="section">
-        <AverageSessionsChart data={userAverageSessions.sessions} />
-      </div>
+        <div className="profile-header">
+          <h1>Bonjour <span className="user-name">{userData.userInfos.firstName}</span></h1>
+          <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+        </div>
+        <div className="section-activity">
+          <ActivityChart data={userActivity.sessions} />
+        </div>
+        <div className='trois-sections'>
+          <div className="section">
+            <AverageSessionsChart data={userAverageSessions.sessions} />
+          </div>
+          <div className="section">
+            <RadarChart data={userPerformance.data} />
+          </div>
+          <div className="section">
+          <ScoreChart data={userData.todayScore} />
+        </div>
+        </div>
       </div> 
       <div className="section">
         <NutritionalInfo
