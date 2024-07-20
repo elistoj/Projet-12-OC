a68892a748b46c123c1b734f4d771d6ec9a2bd57
elistoj <atanastojaneli@gmail.com>
2024-07-20 18:37:07 +0200

    npm

--- back-end/app/index.js
+++ back-end/app/index.js
@@ -7,6 +7,16 @@ const app = express()
 app.use(cors())
 const port = 3000
 
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
+const path = require('path');
+
+// Serviranje favicon.ico
+app.get('/favicon.ico', (req, res) => {
+    res.sendFile(path.join(__dirname, 'favicon.ico'));
+});
+
 app.use(router)
 
 app.listen(port, () => console.log(`Magic happens on port ${port}`))
